80cca56dc7c15ca0c1bc365d3a50e860d8cf0c95
Nestor <nhrivera98@gmail.com>
2020-07-15 19:04:35 -0500

    fixed scrolling glitch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import $ from "jquery";
 // import Preloader from "./Components/Preloader";
 import Navigation from "./Components/Navigation";
 import Home from "./Components/Home";
@@ -10,32 +9,15 @@ import Contact from "./Components/Contact";
 import Social from "./Components/Social";
 
 const App = () => {
-  $(document).ready(function () {
-    $("a").on("click", function (event) {
-      if (this.hash !== "") {
-        event.preventDefault();
-        let hash = this.hash;
-        $("html, body").animate(
-          {
-            scrollTop: $(hash).offset().top,
-          },
-          0,
-          function () {
-            window.location.hash = hash;
-          }
-        );
-      }
-    });
-  });
   return (
-    <div>
+    <>
       <Navigation />
       <Home />
       <AboutMe />
       <Projects />
       <Contact />
       <Social />
-    </div>
+    </>
   );
 };
 
--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,7 +1,40 @@
 import React from "react";
 import "../Styling/navigation.css";
+import $ from "jquery";
 
 const Navigation = () => {
+  $(document).ready(function () {
+    // Add smooth scrolling to all links
+    $("a").on("click", function (event) {
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        let hash = this.hash;
+
+        //Setting scroll-snap-type to none to allow smooth scrolling animation
+        $("html").css({
+          "scroll-snap-type": "none",
+        });
+
+        // Using jQuery's animate() method to add smooth page scroll
+        $("html, body").animate(
+          {
+            scrollTop: $(hash).offset().top,
+          },
+          800,
+          function () {
+            // Add hash (#) to URL when done scrolling (default click behavior) and reverting scroll-snap-type to y mandatory.
+            window.location.hash = hash;
+            $("html").css({
+              "scroll-snap-type": "y mandatory",
+            });
+          }
+        );
+      }
+    });
+  });
   return (
     <div className="navContainer">
       <div className="name">
